d7a2521e779638f818ee744395438da91d0fd827
Michael <michaesine@gmail.com>
2022-03-20 11:27:31 +0100

    Made update function
    Made to update specific records in the mongo instance

--- loyalty.py
+++ loyalty.py
@@ -6,7 +6,7 @@ try:
     conn = MongoClient()
     db = conn.loyalty
     coll = db.loyalty
-    conn.admin.command('ismaster')
+    # conn.admin.command('ismaster')
     print("Mongo connection initiated successfully.")
 except ConnectionFailure:
     print("Could not connect to MongoDB")
@@ -38,11 +38,24 @@ def find(name):
         time.sleep(2)
 
 
+def update(option, old, new):
+    if option == 1:
+        where = {"name": old}
+        what = {"$set": {"name": new}}
+        coll.update_one(where, what)
+    elif option == 2:
+        where = {"number": old}
+        what = {"$set": {"number": new}}
+        coll.update_one(where, what)
+    else:
+        return "Please select one option."
+
+
 while a:
     print("1. Register Account")
     print("2. Find Code")
     print("3. Delete Account")
-    print("3. Change Account\n")
+    print("4. Change Account\n")
     option = int(input("Please select an option:\n) "))
     if option == 1:
         print("register")
@@ -58,12 +71,16 @@ while a:
         print("delete")
         name = input("What is your name?\n) ")
     elif option == 4:
-        print("1. Change Name\n")
+        print("1. Change Name")
         print("2. Change Phone Number\n")
         option = int(input("Please select an option:\n) "))
         if option == 1:
-            print(1)
+            curr_name = input("What is your current name?\n) ")
+            new_name = input("What is your new name?\n) ")
+            update(option, curr_name, new_name)
         elif option == 2:
-            print(2)
+            curr_num = input("What is your current number?\n) ")
+            new_num = input("What is your new number?\n) ")
+            update(option, curr_num, new_num)
     else:
         print("Please select ONE of the FOUR options.")
